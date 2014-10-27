b6c494974abb2fd89cdaa84c0d479e81b98280c5
Apurva Kumar Sinha <ApurvaSinha17@users.noreply.github.com>
2014-10-27 00:26:12 -0400

    Update StringCompression.java
    Added comments

--- StringExp5/src/interview/StringCompression.java
+++ StringExp5/src/interview/StringCompression.java
@@ -14,12 +14,21 @@ public class StringCompression {
 		String compressedString = strComp.returnCompressedString(str);
 		System.out.println("Compressed String: "+compressedString);
 	}
-
+	
+	/*******************************************************************************************
+	 * This method takes the user entered string and returns either
+	 * a new different type of string if there is even one non unique alphabet in that string or
+	 * same string if all the characters are unique
+	 * Example: String will be modified in the following way
+	 * HelloWorld (Input String)
+	 * H1e1l3o2W1r1d1 (Output String)
+	 * ****************************************************************************************/
 	public String returnCompressedString(String str){
 		
 		char[] strChars = str.toCharArray();
 		int strLength = str.length();
 		LinkedHashMap charMap = new LinkedHashMap();
+		//Key: Unique Alphabet; Value: Occurence of that alphabet in a string
 		for(int i=0;i<strLength;i++){
 			if(charMap.containsKey(strChars[i])){
 				int prevCount = (int) charMap.get(strChars[i]);
