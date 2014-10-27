926cd13f2725c66432fe1bb6801a478c8086bd82
Apurva Kumar Sinha <apurva.sinha.17@outlook.com>
2014-10-27 00:36:25 -0400

    Added string length checking logic and modified comments

--- StringExp5/src/interview/StringCompression.java
+++ StringExp5/src/interview/StringCompression.java
@@ -15,20 +15,20 @@ public class StringCompression {
 		System.out.println("Compressed String: "+compressedString);
 	}
 	
-	/*******************************************************************************************
+	/**************************************************************************************************
 	 * This method takes the user entered string and returns either
-	 * a new different type of string if there is even one non unique alphabet in that string or
-	 * same string if all the characters are unique
-	 * Example: String will be modified in the following way
-	 * HelloWorld (Input String)
-	 * H1e1l3o2W1r1d1 (Output String)
-	 * ****************************************************************************************/
+	 * compressed string if the length of the compressed string is smaller than the original string or
+	 * same string if the length of the compressed string is equal or greater than the original string
+	 * Example: String compression will be done in the following way
+	 * aaabbbccaaabcdba (Input String)
+	 * a7b5c3d1 (Output String)
+	 * ************************************************************************************************/
 	public String returnCompressedString(String str){
 		
 		char[] strChars = str.toCharArray();
 		int strLength = str.length();
 		LinkedHashMap charMap = new LinkedHashMap();
-		//Key: Unique Alphabet; Value: Occurence of that alphabet in a string
+		//Key: Unique Alphabet; Value: Occurrence of that alphabet in the string
 		for(int i=0;i<strLength;i++){
 			if(charMap.containsKey(strChars[i])){
 				int prevCount = (int) charMap.get(strChars[i]);
@@ -53,7 +53,7 @@ public class StringCompression {
 		}
 		
 		String compressedString  = strBuilder.toString();
-		if(!flag)
+		if(!flag || compressedString.length()>=str.length())
 			return str;
 		else
 			return compressedString;
